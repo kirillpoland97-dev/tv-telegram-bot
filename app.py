7512677694e4a9ef334e7a9aa1c83e96201bf305
kirillpoland97-dev <kirill.poland97@gmail.com>
2026-03-02 23:20:36 +0300

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, request
 import requests
 import os
 
-# Исправлено: добавлены подчеркивания name
 app = Flask(__name__)
 
-# Проверь названия в Render (Environment): если там BOT_TOKEN, исправь ниже на BOT_TOKEN
 TOKEN = os.environ.get("TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
@@ -15,26 +13,20 @@ def home():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # Получаем данные от TradingView
     data = request.json
 
     if not data:
-        return "no data", 400
+        return "no data"
 
-    # Берем текст сообщения из поля "message"
     message = data.get("message", "Signal")
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
     payload = {
         "chat_id": CHAT_ID,
         "text": message
     }
 
-    # Отправка запроса в Telegram
     requests.post(url, json=payload)
-    
-    return "ok", 200
 
-# Исправлено: добавлены подчеркивания name == "__main__"
-if name == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    return "ok"
